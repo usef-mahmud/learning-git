a28fb96b323de026e0be55059d3fefabae7a8d6e
--replace-all <usef.mahmoud@outlook.com>
2024-05-13 12:04:33 +0300

    print numbers range

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import time
+from utils import get_list
 
 time.sleep(1)
+a = int(input('range start: '))
+b = int(input('range end: '))
 
-for i in range(10):
+for i in get_list(a, b):
     print(f'line number #{i}')
---
+++ utils.py
@@ -0,0 +1,2 @@
+def get_list(a: int, b: int) -> list:
+    return list(range(a, b+1))
